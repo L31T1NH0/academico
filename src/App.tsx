06b32df459c7a53b6476506fe3da54faa0329f0c
L31T1NH0 <oLeiteG@hotmail.com>
2023-04-14 17:13:32 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,34 +17,48 @@ const Candidatos: React.FC = () => {
   };
 
   return (
-    <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-4 mt-16 mb-16 mx-6">
-      {candidatos.CandidatosG.map((candidato: Candidato, index: number) => (
-        <div
-          key={index}
-          className="bg-gray-100 hover:bg-gray-50 rounded text-center 
-          shadow-inner hover:shadow-xl p-4 hover:scale-105 transition-all cursor-pointer border"
-          onClick={() => handleClick(candidato)}
-        >
-          <img
-            src={candidato.foto}
-            alt={candidato.name}
-            className="w-32 h-32 rounded-xl mx-auto mb-4"
-          />
-          <h3 className="text-xl font-bold text-gray-800 mb-2">
-            {candidato.name}
-          </h3>
-          <p className="text-gray-600 line-clamp-4">{candidato.proposta}</p>
-        </div>
-      ))}
+    <div
+      className="
+    grid 
+    sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5
+    gap-4 my-16 mx-4 max-w-max max-h-48"
+    >
+      {candidatos.CandidatosG.sort((a, b) => a.name.localeCompare(b.name)).map(
+        (candidato: Candidato, index: number) => (
+          <div
+            key={index}
+            className="
+            hover:ring hover:ring-emerald-400 rounded text-center 
+            p-4 transition-all cursor-pointer border
+            "
+            onClick={() => handleClick(candidato)}
+          >
+            <div className="flex items-center">
+              <img
+                src={candidato.foto}
+                alt={candidato.name}
+                className="w-24 h-24 rounded mb-4"
+              />
+              <h3 className="text-xl font-bold text-gray-800 mb-2 flex mx-auto">
+                {candidato.name}
+              </h3>
+            </div>
+            <p className="text-gray-600 line-clamp-4">{candidato.proposta}</p>
+          </div>
+        )
+      )}
       {modalOpen && (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-60 flex justify-center items-center">
-          <div className="bg-white rounded-lg p-8 mx-12">
-            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+        <div 
+        className="fixed inset-0 bg-gray-600/60 flex justify-center items-center overflow-y-auto max-h-screen">
+          <div className="bg-white rounded-lg p-8 mx-12 my-16 max-w-4xl max-h-screen">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4 ">
               {candidatoSelecionado?.name}
             </h2>
-            <p className="text-gray-600 mb-4">
-              {candidatoSelecionado?.proposta}
-            </p>
+            <div className="overflow-y-auto max-h-screen">
+              <p className="text-gray-600 mb-4 shadow-inner rounded p-2 mx-2 overflow-y-auto">
+                {candidatoSelecionado?.proposta}
+              </p>
+            </div>
             <button
               className="bg-emerald-500 hover:bg-emerald-700 transition-all text-white py-2 px-4 rounded-lg"
               onClick={() => setModalOpen(false)}
